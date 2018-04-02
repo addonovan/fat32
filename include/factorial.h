7bf8315bbd02366d3b1a5d7b105b2cf5b1116f3b
addonovan <austin@addonovan.com>
2018-04-02 15:00:04 -0500

    Removed factorial things
    No longer needed for testing

--- include/factorial.h
+++
@@ -1,7 +0,0 @@
-#ifndef __FACTORIAL_H__
-#define __FACTORIAL_H__
-
-unsigned int factorial( unsigned short n );
-
-#endif
-
--- src/factorial.c
+++
@@ -1,8 +0,0 @@
-#include <factorial.h>
-
-unsigned int factorial( unsigned short n )	
-{
-    if ( n <= 1 ) return 1;
-    return n * factorial( n - 1 );
-}
-
