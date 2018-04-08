a7c7bb901289021e58e5dcf5303be65448285f3e
addonovan <austin@addonovan.com>
2018-04-08 17:27:11 -0500

    Made input_matching tests more rigorous

--- test/input_matching.c
+++ test/input_matching.c
@@ -9,6 +9,7 @@ int main()
     input_t input;
     input_read( &input, input_matching );
 
+    // test filling 4 ints
     int a, b, c, d;
     require( "%d", 4, input_matches( &input, "test %d %d %d %d\n", &a, &b, &c, &d ) );
     require( "%d", 1, a );
@@ -16,6 +17,7 @@ int main()
     require( "%d", 3, c );
     require( "%d", 4, d );
 
+    // test filling a string
     char e[ INPUT_MAX_LINE_LENGTH ];
     require( "%d", 4, input_matches( &input, "test %d %d %d %s", &a, &b, &c, &e ) );
     require( "%d", 1, a );
@@ -24,6 +26,7 @@ int main()
     require( "%d", 4, d );
     str_require( "4", e );
 
+    // test filling a following (nonexistent) string
     memset( e, '\0', sizeof( e ) );
     require( "%d", 4, input_matches( &input, "test %d %d %d %d %s", &a, &b, &c, &d, &e ) );
     require( "%d", 1, a );
@@ -31,6 +34,19 @@ int main()
     require( "%d", 3, c );
     require( "%d", 4, d );
     str_require( "", e );
+
+    // NEGATIVE
+    // if the leading strings aren't matched, then it shouldn't match anything
+    require( "%d", 0, input_matches( &input, "bad test %d %d %d %d", &a, &b, &c, &d ) );
+
+    // NEGATIVE
+    // if the string is only partially matched, then it should stop after
+    // everything up to the correct point
+    a = b = c = d = -1;
+    require( "%d", 2, input_matches( &input, "test %d %d 5 %d", &a, &b, &d ) );
+    require( "%d", 1, a );
+    require( "%d", 2, b );
+    require( "%d", -1, d );
 
     // check the rest of the file
     char file_name[ INPUT_MAX_LINE_LENGTH ];
