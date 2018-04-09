d386650c0a0b43c4831abae2e538a8f9a6690417
addonovan <austin@addonovan.com>
2018-04-08 20:15:26 -0500

    Made command aliases work

--- include/command.h
+++ include/command.h
@@ -4,6 +4,11 @@
 #include <filesystem.h>
 #include <stdbool.h>
 
+/**
+ * Tries to run all of the built in commands.
+ *
+ * Returns if any of the built-in commands were invoked.
+ */
 bool try_commands( filesystem_t** fs, int argc, char** argv );
 
 #endif
